07999b55ee44c228993cf0b111ffce188a43e4b1
Douglas Batista Santos <dbatists@everis.com>
2020-04-13 09:52:51 -0300

    finalizando poc de webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,12 +2,24 @@ const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 
 
 let plugins = []
+
+plugins.push(new HtmlWebpackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true,
+    },    
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
+
 plugins.push(new extractTextPlugin('styles.css'));
 plugins.push(
     new webpack.ProvidePlugin({
@@ -15,9 +27,18 @@ plugins.push(
         'jQuery': 'jquery/dist/jquery.js'
     })
 );
+plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.bundle.js'
+    }));
+
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
 
 if (process.env.NODE_ENV == "prod") {
-    
+
+    SERVICE_URL = JSON.stringify('http://endereco-prdo');
+
     plugins.push(new babiliPlugin());
 
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
@@ -33,12 +54,18 @@ if (process.env.NODE_ENV == "prod") {
     }));
 }
 
+plugins.push(new webpack.DefinePlugin({
+    SERVICE_URL
+}));
+
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
